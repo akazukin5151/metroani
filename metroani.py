4646e7c590993ffdfc9cc072808e1e043c228c61
twenty5151 <tsuiyikching@protonmail.com>
2020-12-17 15:45:31 +0000

    Move constants to json

--- metroani.py
+++ metroani.py
@@ -10,11 +10,6 @@ import moviepy.video.fx.all as vfx
 from cytoolz import curry, sliding_window
 
 
-# Constants that can be edited
-H, W = 1080, 1920  # dimensions of the video
-DURATION = 0.7  # duration of a single transition
-
-
 # Functions of time that draws animation frames
 
 def show_text_scaler(t, duration):
@@ -55,9 +50,9 @@ def make_text_frames(
     return surface
 
 
-def make_text_frames_from_setting(t, duration, surface, settings, old, new):
+def make_text_frames_from_setting(t, constants, surface, settings, old, new):
     make_text_frames(
-        t, duration, surface, new.name, old.name,
+        t, constants['duration'], surface, new.name, old.name,
         settings.xy, new.font, old.font,
         new.fontsize, old.fontsize,
         old.scale_x, new.scale_x, new.enter_xy,
@@ -68,24 +63,24 @@ def make_text_frames_from_setting(t, duration, surface, settings, old, new):
 
 @curry
 def make_frames(
-    t, duration, settings, next_settings, terminal_settings,
+    t, constants, settings, next_settings, terminal_settings,
     old, new, old_next, new_next, old_term, new_term
 ):
     '''Returns the frames from the transition of three texts as a function of time'''
-    surface = gz.Surface(W,H, bg_color=(1,1,1))
+    surface = gz.Surface(constants['width'], constants['height'], bg_color=(1,1,1))
 
     make_text_frames_from_setting(
-        t, duration, surface, settings,
+        t, constants, surface, settings,
         old, new
     )
 
     make_text_frames_from_setting(
-        t, duration, surface, next_settings,
+        t, constants, surface, next_settings,
         old_next, new_next
     )
 
     make_text_frames_from_setting(
-        t, duration, surface, terminal_settings,
+        t, constants, surface, terminal_settings,
         old_term, new_term
     )
     return surface.get_npimage()
@@ -94,17 +89,17 @@ def make_frames(
 # Animation functions
 
 
-def animate(settings, next_settings, terminal_settings, duration=DURATION):
+def animate(settings, next_settings, terminal_settings, constants):
     '''Animates a transition between two languages'''
     return [
         mpy.VideoClip(
             make_frames(
-                duration=duration, settings=settings,
+                constants=constants, settings=settings,
                 next_settings=next_settings, terminal_settings=terminal_settings,
                 old=names[0], new=names[1], old_next=next_[0], new_next=next_[1],
                 old_term=terminal[0], new_term=terminal[1]
             ),
-            duration=duration
+            duration=constants['duration']
         )
 
         for names, next_, terminal in zip(
@@ -114,33 +109,35 @@ def animate(settings, next_settings, terminal_settings, duration=DURATION):
     ]
 
 
-def combine_language_transitions(station_setting, state_setting, terminal_settings):
+def combine_language_transitions(
+    station_setting, state_setting, terminal_settings, constants
+):
     '''Combines multiple language transitions for a given train state'''
     return mpy.concatenate_videoclips([
-        clip.fx(vfx.freeze, t=DURATION, freeze_duration=1)
+        clip.fx(vfx.freeze, t=constants['duration'], freeze_duration=1)
             .fx(vfx.freeze, t=0, freeze_duration=1)
-        for clip in animate(station_setting, state_setting, terminal_settings)
+        for clip in animate(station_setting, state_setting, terminal_settings, constants)
     ])
 
 
-def combine_train_states(station_setting, state_settings, terminal_settings):
+def combine_train_states(station_setting, state_settings, terminal_settings, constants):
     '''Combines multiple train states and multiple language transitions'''
     return [
         combine_language_transitions(
-            station_setting, state_setting, terminal_settings
+            station_setting, state_setting, terminal_settings, constants
         )
         for state_setting in state_settings
     ]
 
 
 def write_video(
-    filename, stations_settings, state_settings, terminal_settings,
+    filename, stations_settings, state_settings, terminal_settings, constants,
     codec='libx264', fps=60
 ):
     final = []
     for station_setting in station_settings:
         final.append(combine_train_states(
-            station_setting, state_settings, terminal_settings
+            station_setting, state_settings, terminal_settings, constants
         ))
 
     flatten = [clip for station_clips in final for clip in station_clips]
@@ -232,6 +229,7 @@ def all_settings_from_json(file_):
         settings = json.load(f)
 
     return (
+        settings['constants'],
         Transition.from_json_list(settings, 'stations'),
         Transition.from_json(settings, 'terminal'),
         Transition.from_json(settings['states'], 'next'),
@@ -244,11 +242,11 @@ def all_settings_from_json(file_):
 if __name__ == '__main__':
     # TODO: skip transition for states and terminal; only station name has hiragana
     # Can read settings from json file instead
-    (station_settings, terminal_settings, next_settings, arriving_settings,
-        stopping_settings) = all_settings_from_json('settings.json')
+    (constants, station_settings, terminal_settings, next_settings,
+        arriving_settings, stopping_settings) = all_settings_from_json('settings.json')
 
     # Make animation
     write_video(
         'metroani.avi', station_settings, (next_settings, arriving_settings,
-        stopping_settings), terminal_settings
+        stopping_settings), terminal_settings, constants
     )
