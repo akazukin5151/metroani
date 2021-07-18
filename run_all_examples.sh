f4435ecb8dfdc422a129907c9b264de7f35fbacc
twenty5151 <tsuiyikching@protonmail.com>
2021-07-18 23:20:06 +0100

    Rename main.py to dev.py and add run_all_examples.sh

---
+++ run_all_examples.sh
@@ -0,0 +1,7 @@
+python examples/den_en_toshi.py
+python examples/gif.py
+python examples/joban.py
+python examples/keihin.py
+python examples/webm.py
+# Doesn't actually produce a file; just a sanity check
+python examples/python_settings.py
